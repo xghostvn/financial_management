da3d09c2dc667b3ff4d3116bf7dc655204974e98
Tunglx <xghostvn47@gmail.com>
2023-11-29 06:13:17 +0700

    update appspec

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-docker rm -f be_service
-docker-compose -f /var/www/html/docker-compose.yml up -d --build
-docker exec -it be_service php /usr/local/bin/composer install -d /var/www
+systemctl restart nginx.service
